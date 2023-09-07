288d0f803b2fb128c68d31748bd86127219573d4
Danyal <mr.d.bani.d@gmail.com>
2023-09-08 01:09:10 +0330

    init

---
+++ components/BookContainer.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<Card v-for="item in cards" :title="item.title" />
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			cards: [
+				{
+					title: "item1",
+				},
+				{
+					title: "item2",
+				},
+			],
+		};
+	},
+};
+</script>
