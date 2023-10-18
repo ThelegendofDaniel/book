927cfb772f97269af7d71e4b8c582c491ef46d62
Danyal <mr.d.bani.d@gmail.com>
2023-10-18 15:20:16 +0330

    active class change

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -51,7 +51,7 @@
 					>
 						<li>
 							<router-link
-								to="#"
+								to="/"
 								class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
 								aria-current="page"
 								>Home</router-link
@@ -59,21 +59,21 @@
 						</li>
 						<li>
 							<router-link
-								to="#"
+								to="library"
 								class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 								>Search</router-link
 							>
 						</li>
 						<li>
 							<router-link
-								to="#"
+								to="/library"
 								class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 								>Library</router-link
 							>
 						</li>
 						<li>
 							<router-link
-								to="#"
+								to="/account"
 								class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 								>Account</router-link
 							>
@@ -84,3 +84,9 @@
 		</nav>
 	</header>
 </template>
+
+<style>
+.router-link-active {
+	color: rgb(29 78 216);
+}
+</style>
